1b5f02e15df5f064c422b78aae530441c53a390d
VijayMakwana <vijay.makwana@tinyscript.in>
2026-07-17 10:00:15 +0530

    Add: Add tailwind css in to frontend side and configure tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  // server: {
+  //   port: 5173,
+  //   strictPort: true,
+  //   open: true,       // Auto-open browser
+  //   host: true,       // Allow network access
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5500',  // your backend URL
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // }
 })
