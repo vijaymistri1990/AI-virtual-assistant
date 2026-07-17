c36a38a22bfe6daa48a76d0a1cbfe577a21919a4
VijayMakwana <vijay.makwana@tinyscript.in>
2026-07-17 11:04:01 +0530

    Fixed the cors issue in index.js file

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({limit: "32kb" }))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(cors({
-    origin: ["http://localhost:5173"]
+    origin: "*"
 }))
 app.use(cookieParser())
 app.use(express.static("public"))
